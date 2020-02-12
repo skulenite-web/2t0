d6f1f9d1e4abbad490e465c2fa52d6fd52d37d14
Gabe Sher <gabriel.henry.sher@gmail.com>
2020-02-12 13:27:26 -0500

    Updated people roster and updated site structure to fill them in

--- js/fillcreative.js
+++ js/fillcreative.js
@@ -1,19 +1,18 @@
 var ppl = require('js/people.json');
-band = ppl.band;
-
-for (i = 0; i < band.length; i++) {
+brains = ppl.creative;
+for (i = 0; i < brains.length; i++) {
     var boxstr = '<div class="col-sm-6 col-xl-3 mb-4">\n';
     boxstr += '<div class="circ">\n';
     boxstr += '<img src="'
-    boxstr += band[i].photo
-    boxstr += '" class="circ-img" alt="band member photo">\n';
+    boxstr += brains[i].photo
+    boxstr += '" class="circ-img" alt="creative brains photo">\n';
     boxstr += '<div class="circ-body">\n';
     boxstr += '<h5 class="card-title">';
-    boxstr += band[i].name;
+    boxstr += brains[i].name;
     boxstr += '</h5>\n';
     boxstr += '<p class="card-text">'
-    boxstr += band[i].role;
+    boxstr += brains[i].role;
     boxstr += '</p>\n</div>\n</div>\n</div>\n';
-    // alert(band[i].name);
-    document.getElementById("band-people").innerHTML += boxstr;
+    // alert(brains[i].name);
+    document.getElementById("creative-people").innerHTML += boxstr;
 }
